ac73d288c609e1da20d501f2a4362c2fc1d64805
Kwonyoung <rky912@kaist.ac.kr>
2021-02-22 19:58:36 +0900

    add Point_Transformer_SemSeg

--- pointnet2/models/__init__.py
+++ pointnet2/models/__init__.py
@@ -2,3 +2,4 @@ from pointnet2.models.pointnet2_msg_cls import PointNet2ClassificationMSG
 from pointnet2.models.pointnet2_msg_sem import PointNet2SemSegMSG
 from pointnet2.models.pointnet2_ssg_cls import PointNet2ClassificationSSG
 from pointnet2.models.pointnet2_ssg_sem import PointNet2SemSegSSG
+from pointnet2.models.point_transformer_seg import Point_Transformer_SemSeg
