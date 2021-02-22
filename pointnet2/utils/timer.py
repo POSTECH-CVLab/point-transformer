f2fa76ae04b65381469869bd098e0362dd129306
Junha Lee <junhakiwi@postech.ac.kr>
2021-02-22 01:15:10 -0800

    PointTransformer Classification (#10)

--- pointnet2/utils/timer.py
+++ pointnet2/utils/timer.py
@@ -1,13 +1,15 @@
 from time import time
 
 class Timer:
-    def __init__(self, tag):
+    def __init__(self, tag, print=False):
         self.tag = tag
         self.ts = None
+        self.print = print
 
     def tic(self):
         self.ts = time()
 
     def toc(self):
-        print("{}: {}s".format(self.tag, time() - self.ts))
+        if self.print:
+            print("{}: {}s".format(self.tag, time() - self.ts))
         return time()
